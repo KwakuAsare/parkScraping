530f1cde254734ba2288ed4a3c4d9e9352e6262c
Kwaku Asare <lilkwaku55@yahoo.com>
2021-11-17 12:46:21 -0500

    initial commit - importing libraries

---
+++ scraping.js
@@ -0,0 +1,10 @@
+// importing libraries
+const request = require('request');
+const cheerio = require('cheerio');
+const fs = require('fs');
+const writeStream = fs.createWriteStream('terminalOccupancyData.csv');
+
+// Generate database file
+writeStream.write(`Terminal A,Terminal B,Terminal C/D \n`);
+
+var minutes = 45, timerInerval = minutes * 60 * 1000;
