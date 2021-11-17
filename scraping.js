870477ce637f28f6cf3c87126d3a8031e8a36295
Kwaku Asare <lilkwaku55@yahoo.com>
2021-11-17 14:00:56 -0500

    scraping occupancy data

--- scraping.js
+++ scraping.js
@@ -3,8 +3,49 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const writeStream = fs.createWriteStream('terminalOccupancyData.csv');
+const { BigQuery } = require('@google-cloud/bigquery');
 
-// Generate database file
+// Generate database file with fs writestrean
 writeStream.write(`Terminal A,Terminal B,Terminal C/D \n`);
 
+//setting a time interval for scraping
 var minutes = 30, timerInerval = minutes * 60 * 1000;
+
+// Function to scrap Laguardia Parking Occupancy data after every 30 minutes
+function TerminalOccupancyData() {
+
+  //  Scraping using requst npm
+  request('https://www.laguardiaairport.com/to-from-airport/parking', (error, response, html) => {
+    // Check there is no error
+    if (!error && response.statusCode == 200) {
+      // using cheerio library to load the website page html
+      const $ = cheerio.load(html);
+
+      $('.terminal-left').each((span, el) => {
+        // Find the element using the class
+        const terminalA = $(el)
+          .find('.terminal-percentage')
+          .text()
+          .replace(/% Full/, '');
+
+        const terminalB = $(el)
+          .find('.terminal-percentage')
+          .text()
+          .replace(/% Full/, '');
+
+        const terminalCD = $(el)
+          .find('.terminal-percentage')
+          .text()
+          .replace(/% Full/, '');
+      });
+
+      console.log('\nTerminal Data scraped ... \n');
+    }
+
+  });
+
+  // Export to file to upload to database
+  writeStream.write(`${terminalA},${terminalB},${terminalCD} \n`);
+}
+// Run script after every 30 minutes
+setInterval(TerminalOccupancyData, timerInerval);
