c8f754d042a692a1ff8c19b814a41fb987dd63cc
Kwaku Asare <lilkwaku55@yahoo.com>
2021-11-24 13:20:02 -0500

    fixes

--- scraping.js
+++ scraping.js
@@ -1,57 +1,57 @@
 // importing libraries
-//const request = require('request');
-//const cheerio = require('cheerio');
-//const writeStream = fs.createWriteStream('terminalOccupancyData.csv');
+const request = require('request');
+const cheerio = require('cheerio');
+const writeStream = fs.createWriteStream('terminalOccupancyData.csv');
 //const { BigQuery } = require('@google-cloud/bigquery');
 const puppeteer = require('puppeteer')
 const fs = require('fs').promises;
 
 // Generate database file with fs writestrean
-//writeStream.write(`Terminal A,Terminal B,Terminal C/D \n`);
+writeStream.write(`Terminal A,Terminal B,Terminal C/D \n`);
 
 //setting a time interval for scraping
-//var minutes = 30, timerInerval = minutes * 60 * 1000;
+var minutes = 30, timerInerval = minutes * 60 * 1000;
 
-// Function to scrap Laguardia Parking Occupancy data after every 30 minutes
-// function TerminalOccupancyData() {
+//Function to scrap Laguardia Parking Occupancy data after every 30 minutes
+function TerminalOccupancyData() {
 
-//   //  Scraping using requst npm
-//   request('https://www.laguardiaairport.com/to-from-airport/parking', (error, response, html) => {
-//     // Check there is no error
-//     if (!error && response.statusCode == 200) {
-//       // using cheerio library to load the website page html
-//       const $ = cheerio.load(html);
+  //  Scraping using requst npm
+  request('https://www.laguardiaairport.com/to-from-airport/parking', (error, response, html) => {
+    // Check there is no error
+    if (!error && response.statusCode == 200) {
+      // using cheerio library to load the website page html
+      const $ = cheerio.load(html);
 
-//       $('.terminal-left').each((div, el) => {
-//         // Find the element using the class
-//         const terminalA = $(el)
-//           .find('.terminal-percentage')
-//           .text()
-//           .replace(/% Full/, '');
+      $('.terminal-left').each((div, el) => {
+        // Find the element using the class
+        const terminalA = $(el)
+          .find('.terminal-percentage')
+          .text()
+          .replace(/% Full/, '');
 
-//         const terminalB = $(el)
-//           .find('.terminal-percentage')
-//           .text()
-//           .replace(/% Full/, '');
+        const terminalB = $(el)
+          .find('.terminal-percentage')
+          .text()
+          .replace(/% Full/, '');
 
-//         const terminalCD = $(el)
-//           .find('.terminal-percentage')
-//           .text()
-//           .replace(/% Full/, '');
+        const terminalCD = $(el)
+          .find('.terminal-percentage')
+          .text()
+          .replace(/% Full/, '');
 
-//         // Export to file to upload to database
-//         writeStream.write(`${terminalA},${terminalB},${terminalCD} \n`);
-//       });
+        // Export to file to upload to database
+        writeStream.write(`${terminalA},${terminalB},${terminalCD} \n`);
+      });
 
-//       console.log('\nTerminal Data scraped ... \n');
-//     }
+      console.log('\nTerminal Data scraped ... \n');
+    }
 
-//   });
+  });
 
 
-// }
-// // Run script after every 30 minutes
-// setInterval(TerminalOccupancyData, timerInerval);
+}
+// Run script after every 30 minutes
+setInterval(TerminalOccupancyData, timerInerval);
 
 ////////////////////////////////////////////////////////////////////
 
@@ -76,7 +76,7 @@ async function start() {
 
 start()
 
-var minutes = 1, timerInerval = minutes * 60 * 1000;
+var minutes = 30, timerInerval = minutes * 60 * 1000;
 // Run the Main function every 30 minutes
 function scrapeTerminalOccupancyData() {
   // The main function
